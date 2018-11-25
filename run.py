fd7daf97908ddd36416f923d602400d9dfaa1d9d
zephyrtue <s.dhariwal@student.tue.nl>
2018-11-25 15:33:09 +0100

    minor changes

--- run.py
+++ run.py
@@ -5,24 +5,33 @@ debug = False
 mode = 2
 
 if not debug:
-    filename = "./data/ca-AstroPh.txt"
-    M = 1000
+    filename = "./data/out.ca-AstroPh"
+    M_vals = [7000]#[1000, 3000, 5000, 7000, 9000, 10000, 12000]
 else:
     filename = "./data/dummy.txt"
-    M = 7
+    M_vals = [7]
 
-if mode == 1:
-    model = TriestBase(M)
-else:
-    model = TriestImpr(M)
+output_vals = []
 
-with open(filename) as f:
-    for line in f:
-        if line.startswith('#'):
-            continue
+iterations = 10
+
+for M in M_vals:
+    if mode == 1:
+        model = TriestBase(M)
+    else:
+        model = TriestImpr(M)
+
+    with open(filename) as f:
+        for line in f:
+            if line.startswith('%'):
+                continue
         
-        u,v = list(map(int,line.strip().split()))
-        model.run(u,v)
+            u,v = list(map(int,line.strip().split()))
+            model.run(u,v)
 
-    print("============ FINAL OUTPUT IS ====================")
-    print(model.return_counters())
+        print("============ FINAL OUTPUT IS ====================")
+        print(model.return_counters())
+
+        #output_vals.append(model.return_counters()['global'])
+
+#print("Output vals",output_vals)
